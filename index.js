70056b3d5dcc8e87535e2652a1f8699780a14004
danny3819 <danny.nada.djnnr@gmail.com>
2025-05-12 00:21:38 -0600

    se agregan url mas dinamicas y se trabaja mejor :D

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const App = require("express")
-const app = App()
+const express = require("express")
+const app = express()
 
 app.set("view engine", "ejs")
 app.set("views", "./views")
-app.use(App.static("public"))
+app.use(express.static("public"))
+
 
 app.get("/", (req, res) => {
-  res.render("./index")
+  res.render("index")
 })
 
-app.get("/paginas/genialis-UVEG", (req, res) => { 
-  res.render("./paginas/genialis-UVEG")
+app.get("/:carpeta/:materia/:archivo", (req, res) => {
+  const carpeta = req.params.carpeta.replace(/-/g, " ")
+  const materia = req.params.materia.replace(/-/g, " ")
+  const archivo = req.params.archivo
+  const path = `${carpeta}/${materia}/${archivo}`
+
+  res.render(path, (err, html) => {
+    if (err) {
+      res.status(404).send("Página no encontrada")
+    } else {
+      res.send(html)
+    }
+  })
+})
+
+app.get("/paginas/:archivo", (req, res) => {
+  const { archivo } = req.params
+  const path = `paginas/${archivo}`
+
+  res.render(path, (err, html) => {
+    if (err) {
+      console.error(err)
+      res.status(404).send("Página no encontrada")
+    } else {
+      res.send(html)
+    }
+  })
 })
 
 
-//paginas de peticiones para cada materia en esta es introduccion al desarrollo de software
-app.get("/Tipos-de-lenguajes-de-programacion", (req, res) => {
-  res.render("genialis/Introduccion al desarrollo de software/Tipos-de-lenguajes-de-programacion")
-}
-)
-
-app.get("/paginas/algebraING", (req, res) => {
-  res.render("./paginas/algebraING")
-}
-)
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000")
